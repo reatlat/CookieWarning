3c65a64769083c2eda7daf5c0ba06536ef1ff03c
dimitri <developer@dimitros.net>
2013-03-17 00:24:07 +0100

    Cookie path being set to / now and the page is refreshed when a visitor accepts or rejects cookies

--- js/cookiewarning4.js
+++ js/cookiewarning4.js
@@ -17,87 +17,88 @@
 //
 // Download from http://adf.ly/IvElY
 
-function getCookie(c_name)
-{
-var i,x,y,ARRcookies=document.cookie.split(";");
-for (i=0;i<ARRcookies.length;i++)
-  {
-  x=ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
-  y=ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
-  x=x.replace(/^\s+|\s+$/g,"");
-  if (x==c_name)
-    {
-    return unescape(y);
+function getCookie(c_name) {
+    var i, x, y, ARRcookies = document.cookie.split(";");
+    for (i = 0; i < ARRcookies.length; i++) {
+        x = ARRcookies[i].substr(0, ARRcookies[i].indexOf("="));
+        y = ARRcookies[i].substr(ARRcookies[i].indexOf("=") + 1);
+        x = x.replace(/^\s+|\s+$/g, "");
+        if (x == c_name) {
+            return unescape(y);
+        }
     }
-  }
 }
 
 
-function displayNotification()
-{
+function displayNotification() {
 
-// this sets the page background to semi-transparent black should work with all browsers
-var message = "<div id='cookiewarning' >";
+    // this sets the page background to semi-transparent black should work with all browsers
+    var message = "<div id='cookiewarning' >";
 
-// center vert
-	message = message + "<div style='text-align:center;margin:0px;padding:10px;width:100%;background:white;color:black;font-size:90%;'>";
+    // center vert
+    message = message + "<div style='text-align:center;margin:0px;padding:10px;width:100%;background:white;color:black;font-size:90%;'>";
 
-// this is the message displayed to the user.
-message = message + "In order for this site to work correctly, and for us to improve the site we need to store a small file (called a cookie) on your computer.<br/> Most every site in the world does this, however it seems that now, by law, we have to get your permission first.<br/> If you click agree below we will store all needed cookies and you'll notice no diffence, if you click on I don't agree (and while you don't click on I agree) we will not store cookies other than the strictly necessary ones. But in that case you will not be able to post comments, see and use social buttons, etc.<br/>";
-	
-	
-// Displays the I agree/disagree buttons.
-// Feel free to change the address of the I disagree redirection to either a non-cookie site or a Google or the ICO web site 
-message = message + "<br /><INPUT TYPE='button' VALUE='I Agree' onClick='JavaScript:setCookie(\"jsCookieCheck\",null,365);' /> <INPUT TYPE='button' VALUE=\"I don't agree\" onClick='JavaScript:setCookie(\"jsNoCookieCheck\",null,365);' />";
-
-	
-// and this closes everything off.
-message = message + "</div></div>";
+    // this is the message displayed to the user.
+    message = message + "In order for this site to work correctly, and for us to improve the site we need to store a small file (called a cookie) on your computer.<br/> Most every site in the world does this, however it seems that now, by law, we have to get your permission first.<br/> If you click agree below we will store all needed cookies and you'll notice no diffence, if you click on I don't agree (and while you don't click on I agree) we will not store cookies other than the strictly necessary ones. But in that case you will not be able to post comments, see and use social buttons, etc.<br/>";
 
 
-document.writeln(message);
+    // Displays the I agree/disagree buttons.
+    // Feel free to change the address of the I disagree redirection to either a non-cookie site or a Google or the ICO web site 
+    message = message + "<br /><INPUT TYPE='button' VALUE='I Agree' onClick='JavaScript:doAccept();' /> <INPUT TYPE='button' VALUE=\"I don't agree\" onClick='JavaScript:doNotAccept();' />";
+
+
+    // and this closes everything off.
+    message = message + "</div></div>";
+
+
+    document.writeln(message);
 
 
 }
 
-function setCookie(c_name,value,exdays)
-{
-var exdate=new Date();
-exdate.setDate(exdate.getDate() + exdays);
-var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
-document.cookie=c_name + "=" + c_value;
-
-// set cookiewarning to hidden.
-var cw = document.getElementById("cookiewarning");
-cw.innerHTML = "";
+function doAccept() {
+    setCookie("jsCookieCheck", null, 365);
+    location.reload(true);
 }
 
-function checkCookie()
-{
+function doNotAccept() {
+    setCookie("jsNoCookieCheck", null, 365);
+    location.reload(true);
+}
 
-var cookieName="jsCookieCheck";
-var cookieNameNo="jsNoCookieCheck";
-var cookieChk=getCookie(cookieName);
-var cookieChkNo=getCookie(cookieNameNo);
-if (cookieChk!=null && cookieChk!="")
-  {
-  // the jsCookieCheck cookie exists so we can assume the person has read the notification
-  // within the last year
-  
-  setCookie(cookieName,cookieChk,365);	// set the cookie to expire in a year.
-  }
-else if (cookieChkNo!=null && cookieChkNo!="")
-  {
-  // the jsCookieCheck cookie exists so we can assume the person has read the notification
-  // within the last year
-  
-  setCookie(cookieNameNo,cookieChkNo,365);	// set the cookie to expire in a year.
-  }
-else 
-  {
-  // No cookie exists, so display the lightbox effect notification.
-  displayNotification();	
-  }
+function setCookie(c_name, value, exdays) {
+    var exdate = new Date();
+    exdate.setDate(exdate.getDate() + exdays);
+    var c_value = escape(value) + ((exdays == null) ? "" : "; expires=" + exdate.toUTCString() + "; path=/");
+    document.cookie = c_name + "=" + c_value;
+
+    // set cookiewarning to hidden.
+    var cw = document.getElementById("cookiewarning");
+    cw.innerHTML = "";
+}
+
+function checkCookie() {
+
+    var cookieName = "jsCookieCheck";
+    var cookieNameNo = "jsNoCookieCheck";
+    var cookieChk = getCookie(cookieName);
+    var cookieChkNo = getCookie(cookieNameNo);
+    if (cookieChk != null && cookieChk != "") {
+        // the jsCookieCheck cookie exists so we can assume the person has read the notification
+        // within the last year and has accepted the use of cookies
+
+        setCookie(cookieName, cookieChk, 365); // set the cookie to expire in a year.
+    }
+    else if (cookieChkNo != null && cookieChkNo != "") {
+        // the jsNoCookieCheck cookie exists so we can assume the person has read the notification
+        // within the last year and has declined the use of cookies
+
+        setCookie(cookieNameNo, cookieChkNo, 365); // set the cookie to expire in a year.
+    }
+    else {
+        // No cookie exists, so display the lightbox effect notification.
+        displayNotification();
+    }
 }
 
 checkCookie();
