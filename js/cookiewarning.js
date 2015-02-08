83b1a01463cdc0c6a30ebba67801d07ccfa36247
Alex Zappa (Alexander Zapparov) <reatlat@gmail.com>
2015-02-08 15:13:45 +0100

    Update cookiewarning.js

--- js/cookiewarning.js
+++ js/cookiewarning.js
@@ -1,13 +1,3 @@
-/////////////////////////////////////////////////////////////
-//
-// Author Scott Herbert (www.scott-herbert.com)
-//
-// Version History 
-// 1 (19-June-2011) Inital release on to Google code.
-// 1.1 (10-Feb-2013) Released to GitHub
-//
-// Download from http://adf.ly/IvElY
-
 function getCookie(c_name)
 {
 var i,x,y,ARRcookies=document.cookie.split(";");
@@ -22,60 +12,29 @@ for (i=0;i<ARRcookies.length;i++)
     }
   }
 }
-
-
-function displayNotification()
+function displayNotification(c_action)
 {
-
 // this sets the page background to semi-transparent black should work with all browsers
-var message = "<div id='cookiewarning' ><div style='z-index:999; position:absolute; width:100%;height:100%;background: rgb(0, 0, 0) transparent;background: rgba(0, 0, 0, 0.6);filter:progid:DXImageTransform.Microsoft.gradient(startColorstr=#99000000, endColorstr=#99000000);-ms-filter: \"progid:DXImageTransform.Microsoft.gradient(startColorstr=#99000000, endColorstr=#99000000)\"'>";
-
+var message = "<div id='cookiewarning' style='text-align:center;width:100%;background:#333;'>";
 // center vert
-message = message + "<div style='margin:19%;padding:10px;width:62%;height:62%;background:white;color:black'>";
-
-// this is the message displayed to the user.
-message = message + "In order for this site to work correctly, and for us to improve the site we need to store a small file (called a cookie) on your computer.<br/> Most every site in the world does this, however since the 25th of May 2011, by law we have to get your permission first.<br/> If you click agree below we will store cookies and you'll notice no diffence, if you click on I don't agree then this site won't work so we'll re-direct you to Google<br/>";
-	
-	
-// Displays the I agree/disagree buttons.
-// Feel free to change the address of the I disagree redirection to either a non-cookie site or a Google or the ICO web site 
-    message = message + "<br /><INPUT TYPE='button' VALUE='I Agree' onClick='JavaScript:doAccept();' /> <INPUT TYPE='button' VALUE=\"I don't agree\" onClick='JavaScript:doNotAccept("
-	message = message + c_action;
-	message = message + ");' />";
-	
-// and this closes everything off.
-message = message + "</div></div>";
-
+message = message + "<div style='display:inline-block;text-align:left;font-size:14px;line-height:inherit;padding:10px;margin:auto;width:940px;color:white'>In order for this site to work correctly, need to store a small file (called a cookie) on your device. Most every site in the world does this.</br>By using website www.WEBSITE.com you agree to our use of cookies to enhance your experience.<div style='float:right;margin-top:-10px;'><a href='' style='color:white;font-size:40px' onClick='JavaScript:doAccept();'>&#215;</a></div></div>";
+message = message + "</div>";
 document.writeln(message);
 }
-
 function doAccept() {
     setCookie("jsCookieCheck", null, 365);
     location.reload(true);
 }
-
-function doNotAccept(c_action) {
-
-	if (c_action == 1) {
-		setCookie("jsNoCookieCheck", null, 365);
-		location.reload(true);
-	} else {
-		window.location.replace("https://www.google.com/");
-	}
-}
-
 function setCookie(c_name,value,exdays)
 {
 var exdate=new Date();
 exdate.setDate(exdate.getDate() + exdays);
 var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
 document.cookie=c_name + "=" + c_value;
-
 // set cookiewarning to hidden.
 var cw = document.getElementById("cookiewarning");
 cw.innerHTML = "";
 }
-
 function checkCookie(c_action) {
 
     var cookieName = "jsCookieCheck";
@@ -85,13 +44,11 @@ function checkCookie(c_action) {
     if (cookieChk != null && cookieChk != "") {
         // the jsCookieCheck cookie exists so we can assume the person has read the notification
         // within the last year and has accepted the use of cookies
-
         setCookie(cookieName, cookieChk, 365); // set the cookie to expire in a year.
     }
     else if (cookieChkNo != null && cookieChkNo != "") {
         // the jsNoCookieCheck cookie exists so we can assume the person has read the notification
         // within the last year and has declined the use of cookies
-
         setCookie(cookieNameNo, cookieChkNo, 365); // set the cookie to expire in a year.
     }
     else {
@@ -99,9 +56,7 @@ function checkCookie(c_action) {
         displayNotification(c_action);
     }
 }
-
 // blockOrCarryOn - 1 = Carry on, store a do not store cookies cookie and carry on
 //					0 = Block, redirect the user to a different website (google for example)
-var blockOrCarryOn = 1;
+var blockOrCarryOn = 0;
 checkCookie(blockOrCarryOn);
-
